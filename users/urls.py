308e4fb117cc0a99a4e3bbeacdf38e57eb60257a
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-08-22 17:04:02 +1100

    added endpoint and view for user authentication.

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import register_user
+from .views import register_user, login_user
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-
 urlpatterns = [
     path('', include(router.urls)),
     path('register/', register_user),
+    path('login/', login_user),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,8 @@
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
-
+from django.contrib.auth import authenticate
 
 @api_view(['POST'])
 def register_user(request):
@@ -16,3 +15,26 @@ def register_user(request):
         user = serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_409_CONFLICT)
+
+from django.contrib.auth import get_user_model
+@api_view(['POST'])
+def login_user(request):
+    """
+    Authenticates a user with the given credentials, and returns an access token.
+    """
+    
+    username = request.data.get('username')
+    password = request.data.get('password')
+    
+    User = get_user_model()
+    user = User.objects.get(username=username)
+    
+    if user is not None:
+        if user.check_password(password):
+            # return token here
+            return Response({'message': 'Authentication successful'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'Invalid password.'}, status=status.HTTP_401_UNAUTHORIZED)
+    else:
+        # User doesn't exist or credentials are invalid
+        return Response({'message': 'User account does not exist.'}, status=status.HTTP_401_UNAUTHORIZED)
