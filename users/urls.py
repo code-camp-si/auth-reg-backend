7192fbb93b2e5e4d8ddf55bc94bf55c3271545c2
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-08-22 16:09:58 +1100

    enabled browsable API.

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from .views import register_user
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('register/', register_user),
 ]
