a54cd21e02fd5f90f80928a8780678437c315a71
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-08-22 11:38:58 +1100

    overridden create method in UserSerializer, user objects are created with the UserManager so that passwords are hashed before saved.

--- users/serializers.py
+++ users/serializers.py
@@ -10,3 +10,15 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'password', 'email', 'first_name', 'last_name']
         extra_kwargs = {'password': {'write_only': True}}
+
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        return user
+    
