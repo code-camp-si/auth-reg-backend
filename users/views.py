ea93abab41031334447a7375db993fe954198508
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-08-23 23:15:44 +1100

    updated login_user view to return jwt tokens to user upon authentication.

--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
 from django.contrib.auth import authenticate
+from .utils import get_tokens_for_user
+
 
 @api_view(['POST'])
 def register_user(request):
@@ -16,7 +18,7 @@ def register_user(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_409_CONFLICT)
 
-from django.contrib.auth import get_user_model
+    
 @api_view(['POST'])
 def login_user(request):
     """
@@ -29,7 +31,9 @@ def login_user(request):
     user = authenticate(username=username, password=password)
     
     if user is not None:
-        return Response({'message': 'Authentication successful. Token will be made available soon.'}, status=status.HTTP_200_OK)
+        token = get_tokens_for_user(user)
+        
+        return Response({'message': 'Auth Successful.', 'token': token}, status=status.HTTP_200_OK)
     else:
         # User doesn't exist or credentials are invalid
         return Response({'message': 'Invalid username or password.'}, status=status.HTTP_401_UNAUTHORIZED)
