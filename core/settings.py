c82908eba880d0933a2af0293b5d69f1a3609973
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-09-19 10:03:09 +1100

    added ACTIVATION_TOKEN_LIFETIME variable to settings.py.

--- core/settings.py
+++ core/settings.py
@@ -77,6 +77,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACTIVATION_TOKEN_LIFETIME': timedelta(minutes=2),
 }
 
 ROOT_URLCONF = 'core.urls'
