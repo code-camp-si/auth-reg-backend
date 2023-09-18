89fb808298799909d231f107fef921e53b1ecda8
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-09-19 10:04:26 +1100

    added new url endpoint activate-account/.

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import register_user, login_user, user_profile
+from .views import register_user, login_user, user_profile, activate_account
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', register_user),
     path('login/', login_user),
     path('profile/', user_profile),
+    path('activate-account/', activate_account),
 ]
