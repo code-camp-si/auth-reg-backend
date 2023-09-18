b5b1ebf6d7df620dcaa8e6dda0a70cb5beb10e51
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-09-19 10:06:26 +1100

    updated register_user to return activation token, added view activate_account to handle account activation, added and updated util functions.

--- users/utils.py
+++ users/utils.py
@@ -1,4 +1,8 @@
 from rest_framework_simplejwt.tokens import RefreshToken
+from core import settings
+from datetime import datetime, timedelta
+import jwt
+from django.contrib.auth.models import User
 
 
 def get_tokens_for_user(user):
@@ -8,3 +12,27 @@ def get_tokens_for_user(user):
         'refresh': str(refresh),
         'access': str(refresh.access_token),
     }
+    
+    
+def create_activation_token_for_user(user):
+    exp_time = datetime.utcnow() + settings.SIMPLE_JWT.get('ACTIVATION_TOKEN_LIFETIME')
+    token = jwt.encode(
+            {'user_id': user.id, 'exp': exp_time}, 
+            settings.SECRET_KEY, 
+            algorithm='HS256',
+        )
+    token_lifetime_in_mins = settings.SIMPLE_JWT.get('ACTIVATION_TOKEN_LIFETIME').total_seconds() / 60
+    return token, exp_time, token_lifetime_in_mins
+
+
+
+def decode_activation_token(token):
+    payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+    return payload
+           
+
+def set_user_to_active(id):
+    user = User.objects.get(id=id)
+    user.is_active = True
+    user.save()
+    return user
--- users/views.py
+++ users/views.py
@@ -5,21 +5,58 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer
 from django.contrib.auth import authenticate
-from .utils import get_tokens_for_user
+from .utils import get_tokens_for_user, create_activation_token_for_user, set_user_to_active, decode_activation_token
+import jwt
+
 
 
 @api_view(['POST'])
 def register_user(request):
     """
-    Register a new user with the given credentials.
+    Register a new user with the given credentials, and returns an activation token.
     """
     
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         user = serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        activation_token, exp_time, token_lifetime_in_mins = create_activation_token_for_user(user)
+        
+        payload = {
+            'message': 'account registration was successful. Send this token to user email to activate the account.',
+            'name': user.first_name,
+            'email': user.email,
+            'activation_token': activation_token,
+            'exp_time': exp_time,
+            'token_lifetime_in_mins': token_lifetime_in_mins,
+        }
+        
+        return Response(data=payload, status=status.HTTP_201_CREATED)
+    
     return Response(serializer.errors, status=status.HTTP_409_CONFLICT)
 
+
+
+@api_view(['GET'])
+def activate_account(request):
+    """
+    Recieves a token as param and activates user account tied to that token.
+    """
+    
+    if request.GET.get('token') is None:
+        return Response({'message': 'bad request. no token provided.'}, status=status.HTTP_400_BAD_REQUEST) 
+    
+    token = request.GET.get('token')
+    try:
+        payload = decode_activation_token(token)       
+    except jwt.DecodeError:
+        return Response({'message': 'bad request. token has been tampered with.'}, status=status.HTTP_400_BAD_REQUEST) 
+    except jwt.exceptions.ExpiredSignatureError:
+        return Response({'message': 'token has expired.'}, status=status.HTTP_403_FORBIDDEN)
+
+    user = set_user_to_active(payload['user_id'])
+    return Response({'message': f'account for {user.email} has been activated.'}, status=status.HTTP_200_OK)
+   
     
 @api_view(['POST'])
 def login_user(request):
