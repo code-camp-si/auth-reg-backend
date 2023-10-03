7c50a169eaa57635658b0cf5c97d20071ce1dc3a
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2023-10-03 18:03:39 +1100

    added util functions - link construction + mail sender.

--- users/utils.py
+++ users/utils.py
@@ -1,10 +1,51 @@
 from rest_framework_simplejwt.tokens import RefreshToken
 from core import settings
-from datetime import datetime, timedelta
+from datetime import datetime
 import jwt
 from django.contrib.auth.models import User
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 
+def send_activation_link(name: str, email: str, link: str) -> bool:
+    """
+    Sends an activation link to the provided email. Returns True if mail was successfully sent.
+    """
+
+    subject = 'Auth-Reg Activate your Account: Link Activation'
+    message = f'Hi {name},\nclick on this link to activate your account: \n\n{link}'
+
+    message = Mail(
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to_emails=email,
+        subject=subject,
+        plain_text_content=message,
+    )
+    try:
+        sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
+        response = sg.send(message)
+        return True, response.status_code
+    except Exception as e:
+        return False, str(e)
+
+  
+def get_client_address(request):
+    """
+    Returns the base uri of where request is made from.
+    """
+    
+    return f'{request.scheme}://{request.get_host()}' 
+
+
+def construct_link(address, path, param):
+    """
+    Constructs a url link from a base address, a path, and params.
+    note: incomplete, sufficient for only one param. In future, param should be a dict.
+    """
+    
+    return f'{address}{path}?token={param}'
+   
+    
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
 
